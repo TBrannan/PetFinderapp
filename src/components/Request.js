b40b0103d94409c01b8722514e478bf413abb0e5
Travis <glitche@gmail.com>
2021-12-23 13:51:17 -0500

    json looping

--- src/components/Request.js
+++ src/components/Request.js
@@ -11,23 +11,28 @@ class PostList extends React.Component {
   }
 
   componentDidMount() {
+    // for (const [index, element] of data.entries()) {
+    //     console.log(element.package.name);
+    //   }
     // GET request using fetch with set headers
     const headers = { "Content-Type": "application/json" };
     fetch("https://api.npms.io/v2/search?q=react", { headers })
       .then((response) => response.json())
-      .then((data) =>
-        this.setState({ data: data.results[0].package, total: data.total })
-      );
+      .then((data) => this.setState({ data: data.results, total: data.total }));
   }
 
   render() {
     const total = this.state.total;
     const data = this.state.data;
+    const names = [];
+    for (const element of data) {
+      names.push(element.package.name);
+    }
     return (
       <div className="card text-center m-3">
         <h5 className="card-header">GET Request with Set Headers</h5>
         <div className="card-body">total: {total}</div>
-        <div className="card-body">data: {data.name}</div>
+        <div className="card-body">names: {names.join(", ")}</div>
       </div>
     );
   }
